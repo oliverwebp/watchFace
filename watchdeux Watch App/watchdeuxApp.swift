00f6dbea67edb234a371b1ff3635fc840c2dd4fa
oliverwebp <119653688+oliverwebp@users.noreply.github.com>
2023-04-27 02:45:19 -0700

    Initial Commit

---
+++ watchdeux Watch App/watchdeuxApp.swift
@@ -0,0 +1,17 @@
+//
+//  watchdeuxApp.swift
+//  watchdeux Watch App
+//
+//  Created by Oliver Nguyen on 4/27/23.
+//
+
+import SwiftUI
+
+@main
+struct watchdeux_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
